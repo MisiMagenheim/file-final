05a8f57531bcc413b8b910f044a445a6ba635774
Misi <misi.magenheim@gmail.com>
2021-04-20 17:29:50 +0200

    abc

--- src/main/java/com/codecool/examresults/ExamResults.java
+++ src/main/java/com/codecool/examresults/ExamResults.java
@@ -1,10 +1,37 @@
 package com.codecool.examresults;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ExamResults {
     public static List<String> getStudentsNameOrderedByExamResultsAverageFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        List<String> raw;
+        List<Students> students = new ArrayList<>();
+        try {
+            raw = Files.readAllLines(Paths.get(fileName));
+            for (int i = 0; i < raw.size(); i++) {
+                students.add(getStudent(raw.get(i)));
+            }
+        } catch (IOException ioException) {
+            System.out.println("File not found!");
+        }
+        return students.stream()
+                .sorted(Comparator.comparingInt(Students::getPoint).reversed())
+                .map(Students::getName)
+                .collect(Collectors.toList());
     }
-}
+
+    private static Students getStudent(String line) {
+        String[] studentExam = line.split(",");
+        return new Students(studentExam[0],
+                Integer.parseInt(studentExam[1])
+                        + Integer.parseInt(studentExam[2])
+                        + Integer.parseInt(studentExam[3]) / 3);
+    }
+        //TODO your code comes here
+
+    }
+
---
+++ src/main/java/com/codecool/examresults/Students.java
@@ -0,0 +1,21 @@
+package com.codecool.examresults;
+
+public class Students {
+
+        private String name;
+        private int point;
+
+        public Students(String name, int point) {
+            this.name = name;
+            this.point = point;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPoint() {
+            return point;
+        }
+    }
+
